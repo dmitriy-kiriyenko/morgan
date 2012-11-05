5c505cb6c5a33da16bff528bb01b0bbb3ebb83e4
Julia Zhydanova <jz@anahoret.com>
2012-11-05 14:04:22 +0200

    Install unoconv.

--- cookbooks/clearnda-web/recipes/required_packages.rb
+++ cookbooks/clearnda-web/recipes/required_packages.rb
@@ -1,1 +1,15 @@
 package 'libreoffice'
+
+directory "/usr/local/bin/" do
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+remote_file "/usr/local/bin/unoconv" do
+  source "https://raw.github.com/dagwieers/unoconv/3c20d6ec49a97c24cfb1fe9fd61ac80944819088/unoconv"
+  owner "root"
+  group "root"
+  mode "0755"
+  checksum "da8cf55c267c385449a9ffb95d2d8e2b" # A SHA256 (or portion thereof) of the file.
+end
