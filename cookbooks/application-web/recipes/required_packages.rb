b6d051cb9a9cf891abf692d0fce1b2b938e05ecc
Anton Ilin <anton.ilin@anahoret.com>
2012-11-07 12:09:12 +0200

    Clean up artifacts.

--- cookbooks/application-web/recipes/required_packages.rb
+++ cookbooks/application-web/recipes/required_packages.rb
@@ -1,15 +1,18 @@
-package 'libreoffice'
-
-directory "/usr/local/bin/" do
-  owner "root"
-  group "root"
-  mode "0755"
-end
-
-remote_file "/usr/local/bin/unoconv" do
-  source "https://raw.github.com/dagwieers/unoconv/3c20d6ec49a97c24cfb1fe9fd61ac80944819088/unoconv"
-  owner "root"
-  group "root"
-  mode "0755"
-  checksum "da8cf55c267c385449a9ffb95d2d8e2b" # A SHA256 (or portion thereof) of the file.
-end
+# List here packages which need be installed on application server
+# For example:
+#
+# package 'libreoffice'
+#
+# directory "/usr/local/bin/" do
+#   owner "root"
+#   group "root"
+#   mode "0755"
+# end
+#
+# remote_file "/usr/local/bin/unoconv" do
+#   source "https://raw.github.com/dagwieers/unoconv/3c20d6ec49a97c24cfb1fe9fd61ac80944819088/unoconv"
+#   owner "root"
+#   group "root"
+#   mode "0755"
+#   checksum "da8cf55c267c385449a9ffb95d2d8e2b" # A SHA256 (or portion thereof) of the file.
+# end
